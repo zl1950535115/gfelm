32c17613a4bcfef2244d8f3ce433da9794fff93f
zhugaomeng <974203736@qq.com>
2019-06-04 09:01:11 +0800

    123

--- ele/src/api/index.js
+++ ele/src/api/index.js
@@ -5,3 +5,7 @@ console.log('request...', request);
 export let getCateList = params=>{
   return request.get('https://elm.cangdu.org/v2/index_entry?geohash=40.041898,116.300096&group_type=1&flags[]=F')
 }
+
+export let getshoplist = params=>{
+  return request.get('https://elm.cangdu.org/shopping/restaurants?latitude=40.041898&longitude=116.300096&offset=0&limit=20&extras[]=activities&keyword=&restaurant_category_id=&restaurant_category_ids[]=&order_by=&delivery_mode[]=')
+}
--- ele/src/store/modules/index.js
+++ ele/src/store/modules/index.js
@@ -1,17 +1,39 @@
-import {getCateList} from "@/api/index"
+import {getCateList,getshoplist} from "@/api/index"
 
 const state = {
-  list: [1,2,3,4]
+  list: [],
+  List:[]
 }
 
 const mutations = {
-
+  elm(state, payload) {
+    if (payload) {
+      for (const key in payload) {
+        state[key] = payload[key]
+      }
+    }
+  }
 }
 
 const actions = {
-  async getCateList(){
+  async getCateList({commit},payload){
     let data = await getCateList();
-    console.log('data...', data);
+    let len  = data.length
+    let n = 8; //假设每行显示4个
+    let lineNum = len % 8 === 0 ? len / 8 : Math.floor( (len / 8) + 1 );
+    let res = [];
+    for (let i = 0; i < lineNum; i++) {
+    // slice() 方法返回一个从开始到结束（不包括结束）选择的数组的一部分浅拷贝到一个新数组对象。且原始数组不会被修改。
+    let temp = data.slice(i*n, i*n+n);
+    res.push(temp);
+    }
+    console.log(res);
+    commit('elm',{list:res})
+  },
+  async getShoplist({commit},payload){
+    let data = await getshoplist();
+    console.log('123...',data)
+    commit('elm',{List:data})
   }
 }
 
